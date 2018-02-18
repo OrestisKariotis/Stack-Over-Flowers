fa449c1f44580500067a08d9890c99a088a53f57
IoannisVoutsinas <33128851+IoannisVoutsinas@users.noreply.github.com>
2018-02-18 22:54:20 +0200

    Delete ParentDao.java

--- Registration_Login/src/main/java/gr/ntua/ece/softeng/kidspiration/Register_Login/dao/ParentDao.java
+++
@@ -1,8 +0,0 @@
-package gr.ntua.ece.softeng.kidspiration.Register_Login.dao;
-import gr.ntua.ece.softeng.kidspiration.Register_Login.model.Login;
-import gr.ntua.ece.softeng.kidspiration.Register_Login.model.Parent;
-
-public interface ParentDao {
-    void register(Parent parent)
-    Parent validateParent(Login login);
-}
