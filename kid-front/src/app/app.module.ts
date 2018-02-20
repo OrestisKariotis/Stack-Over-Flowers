80f9e74020bddbd743e53b6d095681b1a1b5d6ad
alexvioni <33219746+alexvioni@users.noreply.github.com>
2018-02-20 18:12:13 +0200

    Update app.module.ts

--- kid-front/src/app/app.module.ts
+++ kid-front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,14 @@ import { SearchComponent } from './search/search.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { ParentRegisterComponent } from './parent-register/parent-register.component';
 import { ProviderRegisterComponent } from './provider-register/provider-register.component';
+import { ConfirmPasswordDirective } from './directives/confirm-password.directive';
+
+import { fakeBackendProvider } from './helpers/fakebackend';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { UserLoginService } from './services/user-login.service';
+import { CurrentUserService } from './services/current-user.service';
+
 import { FaqComponent } from './faq/faq.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
@@ -25,6 +34,7 @@ import { TermsComponent } from './terms/terms.component';
     UserLoginComponent,
     ParentRegisterComponent,
     ProviderRegisterComponent,
+    ConfirmPasswordDirective,
     FaqComponent,
     AboutUsComponent,
     ContactComponent,
@@ -34,9 +44,14 @@ import { TermsComponent } from './terms/terms.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserLoginService,
+    CurrentUserService,
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
