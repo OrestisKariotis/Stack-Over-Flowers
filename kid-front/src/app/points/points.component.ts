5c8fc79fd95ee3e979ea5866ccb5598476f58208
Kmitropoulos <kostismitropoulos@gmail.com>
2018-02-24 23:19:59 +0200

    points page

--- kid-front/src/app/points/points.component.ts
+++ kid-front/src/app/points/points.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-points',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PointsComponent implements OnInit {
 
-  constructor() { }
+  model: any = { };
+  commitable: boolean;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.commitable = false;
   }
 
+  setInfo() {
+    this.commitable = true;
+    window.document.getElementById('tab3').click();
+  }
+
+
+  packetSet(num: number) {
+    switch (num) {
+      case 1:
+        this.model.price = 5;
+        this.model.points = 500;
+        break;
+      case 2:
+        this.model.price = 10;
+        this.model.points = 1025;
+        break;
+      case 3:
+        this.model.price = 20;
+        this.model.points = 2100;
+        break;
+      case 4:
+        this.model.price = 50;
+        this.model.points = 5300;
+        break;
+    }
+    window.document.getElementById('tab2').click();
+  }
+
+  buyReset() {
+    this.model = {};
+    this.commitable = false;
+    this.router.navigate(['home']);
+  }
+
+  buySubmit() {
+    // TODO
+  }
 }
