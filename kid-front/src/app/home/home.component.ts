ea1aa5c5251358df305f3183352ebf6a469eabb2
Αλεξάνδρα Βιώνη <alexandravioni@gmail.com>
2018-03-02 19:36:58 +0200

    hello

--- kid-front/src/app/home/home.component.ts
+++ kid-front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '../services/current-user.service';
+import { CurrentUser } from '../models/CurrentUser';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'home';
+  currentUser: CurrentUser;
 
-  constructor() { }
+  constructor(private currentUserService: CurrentUserService) { }
 
   ngOnInit() {
+    this.currentUserService.currentUser.subscribe(user => this.currentUser = user);
+    const prevUser = JSON.parse(sessionStorage.getItem('currentUser')) || {};
+    if (prevUser) {
+      this.currentUserService.changeUser(new CurrentUser(prevUser));
+    }
   }
 
 }
