76f4135ccc93c7ce2f653a30a86d790ac7cd6a5c
johnp41 <32849730+johnp41@users.noreply.github.com>
2018-03-03 11:48:56 +0200

    Add files via upload

---
+++ StringGenerator.java
@@ -0,0 +1,28 @@
+package gr.ntua.ece.softeng.kidspiration;
+
+import java.util.Random;
+
+public class StringGenerator {
+
+
+
+    public String randomgeneratedstring() {
+
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 20;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        // System.out.println(generatedString);
+        return generatedString;
+    }
+
+
+}
